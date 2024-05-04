e26da9d0707207444cd496371f9c16f2f2686422
Vasilii <mr_tomat@mail.ru>
2024-05-04 14:42:26 +0300

    logout is working

--- homework_10/src/myauth/urls.py
+++ homework_10/src/myauth/urls.py
@@ -1,8 +1,10 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from myauth.views import logout_view
 
 app_name = 'myauth'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
--- homework_10/src/myauth/views.py
+++ homework_10/src/myauth/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
