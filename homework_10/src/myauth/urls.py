81bd113489d89e38c45aecfad76640fbd0dedf72
Vasilii <mr_tomat@mail.ru>
2024-05-11 16:44:59 +0300

    profile and registration adding

--- homework_10/src/myauth/urls.py
+++ homework_10/src/myauth/urls.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from myauth.views import logout_view
+from myauth.views import RegisterView, ProfilePage
 
 app_name = 'myauth'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
+    path('register/', RegisterView.as_view(), name="register"),
+    path('profile/', ProfilePage.as_view(), name="profile"),
 ]
--- homework_10/src/myauth/views.py
+++ homework_10/src/myauth/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from django.views.generic import TemplateView
 
 # Create your views here.
 from django.contrib.auth.decorators import login_required
+
+from writers.models import Writer, WriterProfile
+
+def profile(request):
+    # Получаем объект писателя и его профиль из базы данных
+    writer = Writer.objects.first()  # Получите писателя, как вам нужно
+    writer_profile = WriterProfile.objects.first()  # Получите профиль писателя, как вам нужно
+
+    # Передаем данные в шаблон для отображения
+    context = {
+        'writer': writer,
+        'writer_profile': writer_profile,
+    }
+    return render(request, 'profile.html', context)
 
 @login_required
 def dashboard(request):
@@ -12,3 +27,10 @@ def dashboard(request):
 def logout_view(request):
     logout(request)
     return redirect('/')
+
+class RegisterView(TemplateView):
+    template_name = "registration/register.html"
+
+
+class ProfilePage(TemplateView):
+    template_name = "registration/profile.html"
